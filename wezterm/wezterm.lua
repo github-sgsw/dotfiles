ae8b2d096ecf226fa4a4900339f00a0f3dd96f5e
sugi <p20prosugi@gmail.com>
2026-01-25 22:33:57 +0900

    add tab settings change color schema

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,6 +9,9 @@ local config = wezterm.config_builder()
 require("window").setup(wezterm, config)
 require("mouse").setup(wezterm, config)
 require("font").setup(wezterm, config)
+require("tab").setup(wezterm, config)
+config.keys = require("keybinds").keys
+config.key_tables = require("keybinds").key_tables
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
@@ -16,7 +19,7 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Nord (Gogh)'
 
 -- Finally, return the configuration to wezterm:
 return config
