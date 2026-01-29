4c85da11819c546bd4c7fa30190fc02b5607ecc3
sugi <p20prosugi@gmail.com>
2026-01-30 03:59:39 +0900

    デフォルトキーマップから変更

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,15 @@ local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 
+config.leader = { key = 's', mods = 'CTRL', timeout_milliseconds = 2000 }
+
 require("window").setup(wezterm, config)
 require("mouse").setup(wezterm, config)
 require("font").setup(wezterm, config)
 require("tab").setup(wezterm, config)
 require("cursor").setup(wezterm, config)
 config.keys = require("keybinds").keys
-config.key_tables = require("keybinds").key_tables
+--config.key_tables = require("keybinds").key_tables
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
