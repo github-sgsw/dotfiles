83b62ea7823de997bc16a86d6ab40e87b84db873
sugi <p20prosugi@gmail.com>
2026-02-01 20:57:17 +0900

    delete space

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -5,22 +5,22 @@ return {
   keys = {
     -- 画面フルスクリーン切り替え。背景透過が連動しないからいらないかも、
     { key = 'Enter', mods = 'ALT', action = act.ToggleFullScreen },
-    
+
     -- Pane操作
     { key = 'm', mods = 'ALT', action = act.ActivatePaneDirection('Left') },
     { key = 'v', mods = 'ALT', action = act.ActivatePaneDirection('Right') },
     { key = 't', mods = 'ALT', action = act.ActivatePaneDirection('Up') },
     { key = 'w', mods = 'ALT', action = act.ActivatePaneDirection('Down') },
-    
+
     -- ペインの水平分割
     { key = '|', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     -- ペインの上下分割
     { key = '-', mods = 'LEADER', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-    
+
     -- タブ移動
     { key = 'RightArrow', mods = 'ALT', action = act.ActivateTabRelative(1) },
     { key = 'LeftArrow', mods = 'ALT', action = act.ActivateTabRelative(-1) },
-    
+
     -- コマンドパレット表示
     { key = 'p', mods = 'LEADER', action = act.ActivateCommandPalette }
   }
